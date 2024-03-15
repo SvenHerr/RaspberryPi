b05368e534450075bbf45f2d756343b8e971bd56
Sven Herrmann <info@herrmannsven.de>
2024-03-15 21:06:49 +0100

    .

--- UptimeMonitor1.py
+++ UptimeMonitor1.py
@@ -1,94 +1,91 @@
-import json
 import subprocess
-import os, time
-import webservice
+import os
 
-data = {};
-value = {};
+data = {}
+value = {}
 
-def replacePoint(str):
-  temp = str.replace(',','.')
-  return temp
+def replace_point(string):
+    return string.replace(',', '.')
 
-def cast(str):
-  temp = str[:-1]
-  return float(temp)
+def cast(string):
+    return float(string[:-1])
 
-def castAndReplace(str):
-  temp = replacePoint(str)
-  return cast(temp)
+def cast_and_replace(string):
+    temp = replace_point(string)
+    return cast(temp)
 
-def getCPU():
-  CPU_Pct=(round(float(os.popen('''grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage }' ''').readline()),2))
-  global data;
-  data['CPU_USAGE'] = str(CPU_Pct) + "%"
-  value['CPU_USAGE'] = CPU_Pct
+def get_cpu():
+    CPU_Pct = round(float(os.popen('''grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage }' ''').readline()), 2)
+    data['CPU_USAGE'] = f"{CPU_Pct}%"
+    value['CPU_USAGE'] = CPU_Pct
+
 def display(val):
-  return round(float(val),2)
-def getRam():
-  
-  ramTotalGB = round(display(subprocess.check_output("free | awk 'FNR == 2 {print $2/1000000}'", shell=True)))
-  Usage = subprocess.check_output("free | awk 'FNR == 2 {print $3/($3+$4)*100}'", shell=True)
-  
-  
-  global data;
-  data['MEM_TOTAL'] = str(ramTotalGB) + "GB"
-  value['MEM_TOTAL'] = ramTotalGB
-  data['MEM_USAGE'] = str(display(Usage)) + "%"
-  value['MEM_USAGE'] = display(Usage)
-  data['MEM_FREE'] = str(100 - display(Usage)) + "%"
-  value['MEM_FREE'] = 100 - display(Usage)
-def getStorage():
-  
-  storageTotal = subprocess.check_output("df -h --total | awk  '/^total/ {print $2}'", shell=True).rstrip()
-  storageUsed = subprocess.check_output("df -h --total | awk  '/^total/ {print $3}'", shell=True).rstrip()
-  storageFree = subprocess.check_output("df -h --total | awk  '/^total/ {print $4}'", shell=True).rstrip()
-  
-  
-  global data;
-  data['STORAGE_TOTAL'] = str(storageTotal)
-  value['STORAGE_TOTAL'] = castAndReplace(storageTotal)
-  data['STORAGE_FREE'] = str(storageFree)
-  value['STORAGE_FREE'] = castAndReplace(storageFree)
-  data['STORAGE_USAGE'] = str(storageUsed)
-  value['STORAGE_USAGE'] = castAndReplace(storageUsed) 
+    return round(float(val), 2)
 
-def getSys():
-  runningTime = subprocess.check_output("cat /proc/uptime | awk '{print $1}'", shell=True).rstrip()
-  thermal = subprocess.check_output("cat /sys/class/thermal/thermal_zone0/temp", shell=True).rstrip()
-  
-  global data;
-  data['RUNNING_TIME'] = str(round(display(runningTime)/60/60,2))+"Hours"
-  value['RUNNING_TIME'] = round(display(runningTime)/60/60,2)
-  data['TEMPERATURE'] = str(round(float(thermal)/1000,2)) +"C"
-  temp = str(round(float(thermal)/1000,2))
-  value['TEMPERATURE'] = castAndReplace(temp)
-def isProgramRunning():
-  Usage = subprocess.check_output("ps up `cat daemon.pid` >/dev/null && echo 'Running' || echo 'Not running'", shell=True)
-  global data;
-  data['PROGRAM_CHECK'] = Usage
-  #print Usage
+def get_ram():
+    ram_total_gb = round(display(subprocess.check_output("free | awk 'FNR == 2 {print $2/1000000}'", shell=True)))
+    usage = subprocess.check_output("free | awk 'FNR == 2 {print $3/($3+$4)*100}'", shell=True)
+    data['MEM_TOTAL'] = f"{ram_total_gb}GB"
+    value['MEM_TOTAL'] = ram_total_gb
+    data['MEM_USAGE'] = f"{display(usage)}%"
+    value['MEM_USAGE'] = display(usage)
+    data['MEM_FREE'] = f"{100 - display(usage)}%"
+    value['MEM_FREE'] = 100 - display(usage)
+
+def get_storage():
+    storage_total = subprocess.check_output("df -h --total | awk  '/^total/ {print $2}'", shell=True).rstrip().decode('utf-8')
+    storage_used = subprocess.check_output("df -h --total | awk  '/^total/ {print $3}'", shell=True).rstrip().decode('utf-8')
+    storage_free = subprocess.check_output("df -h --total | awk  '/^total/ {print $4}'", shell=True).rstrip().decode('utf-8')
+
+    global data
+    data['STORAGE_TOTAL'] = str(storage_total)
+    value['STORAGE_TOTAL'] = cast_and_replace(storage_total)
+    data['STORAGE_FREE'] = str(storage_free)
+    value['STORAGE_FREE'] = cast_and_replace(storage_free)
+    data['STORAGE_USAGE'] = str(storage_used)
+    value['STORAGE_USAGE'] = cast_and_replace(storage_used)
+
+def cast_and_replace(string):
+    temp = replace_point(string)
+    return cast(temp)
+
+def get_sys():
+    running_time = subprocess.check_output("cat /proc/uptime | awk '{print $1}'", shell=True).rstrip()
+    thermal = subprocess.check_output("cat /sys/class/thermal/thermal_zone0/temp", shell=True).rstrip()
+    data['RUNNING_TIME'] = f"{round(display(running_time)/60/60, 2)}Hours"
+    value['RUNNING_TIME'] = round(display(running_time)/60/60, 2)
+    data['TEMPERATURE'] = f"{round(float(thermal)/1000, 2)}C"
+    value['TEMPERATURE'] = cast_and_replace(str(round(float(thermal)/1000, 2)))
+
+def is_program_running():
+    usage = subprocess.check_output("ps up `cat daemon.pid` >/dev/null && echo 'Running' || echo 'Not running'", shell=True)
+    data['PROGRAM_CHECK'] = usage
+
 if __name__ == "__main__":
-  getCPU();
-  getRam();
-  getStorage();
-  getSys();
-  #isProgramRunning();
-  
-  #print data
-  print "CPU USAGE:\t" + data['CPU_USAGE'];
-  print "MEMORY TOTAL:\t" + data['MEM_TOTAL'];
-  print "MEMORY USAGE:\t" + data['MEM_USAGE'];
-  print "MEMORY USAGE:\t" + data['MEM_FREE'];
-  print "STORAGE TOTAL:\t" + data['STORAGE_TOTAL'];
-  print "STORAGE USAGE:\t" + data['STORAGE_USAGE'];
-  print "STORAGE FREE:\t" + data['STORAGE_FREE'];
-  print "RUNNING TIME:\t" + data['RUNNING_TIME'];
-  print "CPU TEMP:\t" + data['TEMPERATURE'];
+    get_cpu()
+    get_ram()
+    get_storage()
+    get_sys()
 
-try:
- webservice.send('token','Magic Mirror','','1',value['CPU_USAGE'],value['MEM_TOTAL'],value['MEM_USAGE'],value['MEM_FREE'],value['STORAGE_TOTAL'],value['STORAGE_USAGE'],value['STORAGE_FREE'],value['RUNNING_TIME'],value['TEMPERATURE'])
- print(r)
-except:
-  print("Error")
-print("Script erfolgreich")
+    print("CPU USAGE:\t", data['CPU_USAGE'])
+    print("MEMORY TOTAL:\t", data['MEM_TOTAL'])
+    print("MEMORY USAGE:\t", data['MEM_USAGE'])
+    print("MEMORY USAGE:\t", data['MEM_FREE'])
+    print("STORAGE TOTAL:\t", data['STORAGE_TOTAL'])
+    print("STORAGE USAGE:\t", data['STORAGE_USAGE'])
+    print("STORAGE FREE:\t", data['STORAGE_FREE'])
+    print("RUNNING TIME:\t", data['RUNNING_TIME'])
+    print("CPU TEMP:\t", data['TEMPERATURE'])
+
+    # Uncomment the following block if needed
+    '''
+    try:
+        webservice.send('token', 'Magic Mirror', '', '1', value['CPU_USAGE'], value['MEM_TOTAL'], value['MEM_USAGE'],
+                        value['MEM_FREE'], value['STORAGE_TOTAL'], value['STORAGE_USAGE'], value['STORAGE_FREE'],
+                        value['RUNNING_TIME'], value['TEMPERATURE'])
+        print(r)
+    except Exception as e:
+        print("Error:", e)
+    '''
+
+    print("Script erfolgreich")
